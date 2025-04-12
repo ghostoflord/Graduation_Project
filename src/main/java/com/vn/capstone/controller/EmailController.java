241c9f7a92da6341f82e01b5e265b2c2b0e4998a
ghostoflord <ghost2892003zzz@gmail.com>
2025-04-12 16:49:24 +0700

    update send email

---
+++ src/main/java/com/vn/capstone/controller/EmailController.java
@@ -0,0 +1,29 @@
+package com.vn.capstone.controller;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vn.capstone.service.EmailService;
+import com.vn.capstone.util.annotation.ApiMessage;
+
+import jakarta.transaction.Transactional;
+
+@RestController
+@RequestMapping("/api/v1")
+public class EmailController {
+
+    private final EmailService emailService;
+
+    public EmailController(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+    @GetMapping("/email")
+    @ApiMessage("Send simple email")
+    public String sendSimpleEmail() {
+        this.emailService.sendEmailFromTemplate("lovegau2892003@gmail.com", "test send email", "email");
+        return "ok";
+    }
+}
